99da25db38e5f2ddd54c709c9381ca3dc1b01f2d
CyberVenlor <BadCat17@163.com>
2024-07-31 10:22:35 +0800

    add point and spring force analyzer

--- Entity/Entity.cs
+++ Entity/Entity.cs
@@ -10,11 +10,16 @@ public partial class Entity : Area2D { //使用area2d作为零时的AABB箱
         public float Mass;
         public Vector2 Position;
         public Vector2 Velocity;
+
+		public float DistanceTo(Point point) {
+			return Position.DistanceTo(point.Position);
+		}
     }
 	//基础弹簧
 	public struct Spring {
 		public int P1;//point在Entity.Points中的索引
 		public int P2;
+		public float Length;
 		public float Strength;
 		public float Damping;
 	}
@@ -22,14 +27,22 @@ public partial class Entity : Area2D { //使用area2d作为零时的AABB箱
 	public Point[] Points = [];
 	public Spring[] Springs = [];
 
-    //ready
+    //ready and phy
     public override void _Ready() {
 		//初始化点等数据
         if (resEntity != null) InitFromResEntity(resEntity);
 		DrawEntity();
     }
 
-	public void InitFromResEntity(ResEntity resEntity) {
+    public override void _PhysicsProcess(double delta)
+    {
+        SimulateSpring((float)delta);
+		SimulatePoint((float)delta);
+		QueueRedraw();
+    }
+
+	//
+    public void InitFromResEntity(ResEntity resEntity) {
 		//init points
 		Points = new Point[resEntity.Points.Length];
 		for (int i = 0; i < resEntity.Points.Length; i++) {
@@ -39,10 +52,13 @@ public partial class Entity : Area2D { //使用area2d作为零时的AABB箱
 		//init springs
 		Springs = new Spring[resEntity.Springs.Length];
 		for (int i = 0; i < resEntity.Springs.Length; i++) {
-			Springs[i].P1 = resEntity.Springs[i].P1;
-			Springs[i].P2 = resEntity.Springs[i].P2;
-			Springs[i].Strength = resEntity.Springs[i].Strength;
-			Springs[i].Damping = resEntity.Springs[i].Damping;
+			ref Spring spring = ref Springs[i];
+			ResSpring resSpring = resEntity.Springs[i];
+			spring.P1 = resSpring.P1;
+			spring.P2 = resSpring.P2;
+			spring.Length = Points[spring.P1].DistanceTo(Points[spring.P2]) * resSpring.LengthMultiplier;
+			spring.Strength = resSpring.Strength;
+			spring.Damping = resSpring.Damping;
 		}
 	}
 
@@ -56,7 +72,6 @@ public partial class Entity : Area2D { //使用area2d作为零时的AABB箱
 						Points[spring.P2].Position,
 						Colors.White
 					);
-					GD.Print(1);
 				}
 			}
 			//
@@ -66,5 +81,26 @@ public partial class Entity : Area2D { //使用area2d作为零时的AABB箱
 				}
 			}
 		};
+	}
+
+	private void SimulatePoint(float delta) {
+		for (int i = 0; i < Points.Length; i++) {
+			Points[i].Position += Points[i].Velocity * delta;
+		}
+	}
+
+	private void SimulateSpring(float delta) {
+		foreach (Spring spring in Springs) {
+			ref Point p1 = ref Points[spring.P1];
+			ref Point p2 = ref Points[spring.P2];
+
+			Vector2 force = (p2.Position - p1.Position).Normalized()
+				* (p1.DistanceTo(p2) - spring.Length) 
+				* spring.Strength
+				+ (p2.Velocity - p1.Velocity) * spring.Damping;//阻尼
+
+			p1.Velocity += force * delta / p1.Mass;
+			p2.Velocity -= force * delta / p2.Mass;
+		}
 	}
 }
--- Entity/ResSpring.cs
+++ Entity/ResSpring.cs
@@ -5,6 +5,7 @@ using Godot;
 public partial class ResSpring : Resource {
     [Export] public int P1;
     [Export] public int P2;
-    [Export] public float Strength;
-    [Export] public float Damping;
+    [Export] public float LengthMultiplier = 1f;
+    [Export] public float Strength = 1f;
+    [Export] public float Damping = 0.1f;
 }
